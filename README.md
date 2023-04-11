5fdc3c70821c8a3a739b3efec255de2be45b3e2c
严家辉 <crazymryan@gmail.com>
2023-04-11 21:13:01 +0800

    fix: ENV 变量调整

---
+++ README.md
@@ -0,0 +1,16 @@
+# ChatAudio
+语音对话
+
+## 启动
+```
+yarn install && yarn start
+```
+
+## 配置
+```env
+VERCEL_ANALYTICS_ID=""
+OPENAI_API_KEY=""
+TTS_APP_ID=""
+TTS_API_SECRET=""
+TTS_API_KEY=""
+```
--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const fs = require("fs");
 const { promisify } = require("util");
 const path = require("path");
 const tts = promisify(require("./utils/tts"));
-const configPath = process.env.NODE_ENV === 'production' ? `.env.production.local` : '.env.preview.local';
-// console.log(process.env.NODE_ENV);
 
-require("dotenv").config({ path: path.resolve(__dirname, configPath)});
+require("dotenv").config({
+  path: path.resolve(__dirname, ".env.preview.local"),
+});
 
-const openGreetings = (text) => {
+const generateAudio = (text) => {
   return new Promise((resolve, reject) => {
     const auth = {
-      app_id: process.env.TTS_APP_ID, 
-      app_skey: process.env.TTS_API_SECRET, 
-      app_akey: process.env.TTS_API_KEY
+      app_id: process.env.TTS_APP_ID,
+      app_skey: process.env.TTS_API_SECRET,
+      app_akey: process.env.TTS_API_KEY,
     };
     // 讯飞 api 参数配置
     const business = {
@@ -27,18 +27,18 @@ const openGreetings = (text) => {
       volume: 100,
       bgs: 0,
     };
-    const id = new Date().getTime()
+    const id = new Date().getTime();
     // 存储文件的路径
     const file = path.resolve(__dirname, `client/audio/${id}.m4a`);
     try {
       // 执行请求
       tts(auth, business, text, file).then((res) => {
-        resolve(`audio/${id}.m4a`)
+        resolve(`audio/${id}.m4a`);
       });
     } catch (e) {
-      reject(e)
+      reject(e);
     }
-  })
+  });
 };
 
 const configuration = new Configuration({
@@ -61,64 +61,69 @@ const handleIssueReply = async (prompt) => {
   });
   const chat = choices[0].text?.trim();
   console.log("生成的文本内容是>>>", chat);
-  return chat
+  return chat;
+};
+
+const getAudioLength = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.stat(path.resolve(__dirname, `client/${filePath}`), function (err, stats) {
+      if (err) {
+        reject(err);
+      }
+      resolve(parseInt(stats.size / 6600));
+   });
+  })
 }
 
 app.use(fileUpload());
 
-app.post("api/audio", async (req, res) => {
-  if (!req.files) {
-    return res.status(400).send("No files were uploaded.");
-  }
+app.post("/api/audio", async (req, res) => {
+  if (!req.files) return res.status(400).send({ message: "缺少参数", error: true });
 
-  if(req.files) {
-    const file = req.files.file;
-    const fileName = "test.m4a";
-  
-    file.mv(fileName, async (err) => {
-      if (err) {
-        return res.status(500).send(err);
-      }
-  
-      const {
-        data: { text: prompt },
-      } = await openai.createTranscription(
-        fs.createReadStream(fileName),
-        "whisper-1"
-      );
-  
-      console.log("解析的音频内容是>>>", prompt);
-      if(!prompt.trim().length) return res.send({ message: "未识别到语音内容", error: true })
-      
-      const text = await handleIssueReply(prompt);
-      const content = await openGreetings(text);
-      console.log('生成的音频是>>>', content);
-      res.send([
-        { type: "system", content, text, infoType: 'audio', playStatus: false },
-      ]);
-    });
-  }
+  const file = req.files.file;
+  // 存放用户上传的文件
+  const fileName = "audio.m4a";
 
+  file.mv(fileName, async (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    const {
+      data: { text: prompt },
+    } = await openai.createTranscription(
+      fs.createReadStream(fileName),
+      "whisper-1"
+    );
+
+    console.log("解析的音频内容是>>>", prompt);
+    // 判断用户上传音频是否存在内容
+    if (!prompt.trim().length)
+      return res.send({ message: "未识别到语音内容", error: true });
+
+    const chatReply = await handleIssueReply(prompt);
+    console.log("生成的文本内容是>>>", chatReply);
+
+    const content = await generateAudio(chatReply);
+    console.log("生成的音频是>>>", content);
+
+    const length = await getAudioLength(content)
+    console.log("生成的音频长度是>>>", length);
+    
+    res.send([
+      { type: "system", content, chatReply, infoType: "audio", length, playStatus: false },
+    ]);
+  });
 });
 
-app.get('/api/submit-issue', async (req, res) => {
+app.get("/api/submit-issue", async (req, res) => {
   const { issue } = req.query;
-  if(!issue.trim()) return res.send({ message: "缺少参数", error: true })
-  if(issue) {
-    try {
-      const chat = await handleIssueReply(issue);
-      return res.send([
-        { type: "system", content: chat },
-      ]);
-    } catch (error) {
-      res.status(500).send({ error: true, message: error })
-    }
-  }
-})
+  if (!issue.trim()) return res.status(400).send({ message: "缺少参数", error: true });
+  const chatReply = await handleIssueReply(issue);
+  return res.send([{ type: "system", content: chatReply.trim() }]);
+});
 app.use(express.static(path.join(__dirname, "client")));
 
-// app.listen(3000, () => {
-//   // console.log(process.env.OPENAI_API_KEY);
-// });
+app.listen(3000);
 
 module.exports = app;
